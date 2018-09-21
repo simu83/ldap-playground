7ff7950602c6642ab38df7379af2096b26121c5e
simu83 <simon.schaad@students.bfh.ch>
2018-09-21 12:01:46 +0200

    Initial commit

---
+++ src/main/java/ch/bfh/ti/thesis/hs18/notensystemweiterbildungti/ldapplayground/HomeController.java
@@ -0,0 +1,14 @@
+package ch.bfh.ti.thesis.hs18.notensystemweiterbildungti.ldapplayground;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the Playground";
+    }
+
+}
---
+++ src/main/java/ch/bfh/ti/thesis/hs18/notensystemweiterbildungti/ldapplayground/LdapPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package ch.bfh.ti.thesis.hs18.notensystemweiterbildungti.ldapplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapPlaygroundApplication.class, args);
+	}
+}
